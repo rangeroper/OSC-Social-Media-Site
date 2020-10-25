f93e99a513af302e0e45a82f7cdd62d615ec8efd
Brandon Mochel <62531841+brmochel92@users.noreply.github.com>
2020-10-25 12:46:51 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const config = {
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
-      'mongodb://' + (process.env.IP || 'localhost') + 'brandon:gunsammo92@osc-db-shard-00-00.3m16e.mongodb.net:27017,osc-db-shard-00-01.3m16e.mongodb.net:27017,osc-db-shard-00-02.3m16e.mongodb.net:27017/test?replicaSet=atlas-zdnnpy-shard-0&ssl=true&authSource=admin' +
+      'mongodb://' + (process.env.IP || 'localhost') + '' +
     (process.env.MONGO_PORT || '27017') +
     '/mernproject'
 }
